2ea5ba1c8504c3c46e34bc1b029465f0fa1b4178
Turbid <raghuram2432006@gmail.com>
2024-08-15 16:01:26 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const bcrypt = require('bcryptjs');
+const path = require('path');
 const Contact = require('./models/contact'); // Import your Contact model
-const User = require('./models/users')
+const User = require('./models/users');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB Atlas Connection
-const uri = 'mongodb+srv://raghuram2432006:EFmdhacZuFY9eWzx@cluster0.fwyro.mongodb.net/?retryWrites=true&w=majority&appName=cluster0';
+const uri = process.env.MONGODB_CONNECT_URI || 'mongodb+srv://raghuram2432006:EFmdhacZuFY9eWzx@cluster0.fwyro.mongodb.net/?retryWrites=true&w=majority&appName=cluster0';
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const connection = mongoose.connection;
@@ -43,7 +43,15 @@ app.post('/submit-contact-form', async (req, res) => {
         res.status(400).send('Error saving form');
     }
 });
+
 app.use('/api/auth', require('./routes/auth'));
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
