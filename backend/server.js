39256ea31db38735019893b52408c4ac649af46b
Turbid <raghuram2432006@gmail.com>
2024-08-16 11:56:25 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 // MongoDB Atlas Connection
 const uri = process.env.MONGODB_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(uri);
 
 const connection = mongoose.connection;
 connection.once('open', () => {
