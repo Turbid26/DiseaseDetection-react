f1fa7a6e2184df223bc8b2acd90719360436ab56
Turbid <raghuram2432006@gmail.com>
2024-12-13 13:36:23 +0530

    update

--- src/App.js
+++ src/App.js
@@ -9,13 +9,17 @@ import Diagnose from './components/services.tsx';
 import History from './components/history.tsx';
 import FAQ from './components/FAQ.tsx';
 import Blog from './components/blog';
+import { I18n } from 'react-polyglot';
 import { AuthProvider } from './context/AuthContext';
 import ForgotPassword from './components/forgotpassword';
 import ResetPassword from './components/resetpassword';
+import { messages } from './locales'; 
 
 function App() {
+  const language = navigator.language.split('-')[0] || 'en'; // Default to 'en' if no language detected
   return (
     <AuthProvider>
+    <I18n locale={language} messages={messages}>
     <Router>
       <Routes>
         <Route path="/" element={<Login />} /> {/* No Navbar here */}
@@ -30,6 +34,7 @@ function App() {
         <Route path="/resetpassword" element={<ResetPassword />} />
       </Routes>
     </Router>
+    </I18n>
     </AuthProvider>
   );
 }
---
+++ src/locales.js
@@ -0,0 +1,29 @@
+// locales/index.js
+export const messages = {
+  en: {
+    home: 'Home',
+    services: 'Services',
+    history: 'History',
+    community: 'Community',
+    contact: 'Contact',
+    faq: 'FAQ',
+    english: 'English',
+    hindi: 'Hindi',
+    login: 'Login',
+    logout: 'Logout',
+    welcome: 'Welcome',
+  },
+  hi: {
+    home: 'मुख्य पृष्ठ',
+    services: 'सेवाएँ',
+    history: 'इतिहास',
+    community: 'समुदाय',
+    contact: 'संपर्क करें',
+    faq: 'अक्सर पूछे जाने वाले प्रश्न',
+    english: 'अंग्रेजी',
+    hindi: 'हिंदी',
+    login: 'लॉगिन करें',
+    logout: 'लॉगआउट',
+    welcome: 'स्वागत है',
+  }
+};
